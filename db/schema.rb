ac31f38171e1ba20339fe52bc6c6cc89c752037b
Richard Ten <rten04@icloud.com>
2023-06-13 22:20:42 -0400

    Migrate db after creating Attendance model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_181602) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_14_021639) do
+  create_table "attendances", force: :cascade do |t|
+    t.integer "event_id"
+    t.integer "attendee_id"
+    t.boolean "attending"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "name"
     t.string "location"
