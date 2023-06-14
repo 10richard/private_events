877494f50789abe4cc9c9d83adf4de93b6e3ba62
Richard Ten <rten04@icloud.com>
2023-06-14 13:11:26 -0400

    Add routes for Attendances (#create and #destroy)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
 
+  resources :attendances, only: [:create, :destroy]
+
   resources :events, only: [:index, :new, :create, :show]
   root to: 'events#index'
 end
