d20cb2e2c4c23367ea50565d35580980128a8420
Richard Ten <rten04@icloud.com>
2023-06-14 17:23:15 -0400

    Fix #create action

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,16 @@
 class AttendancesController < ApplicationController
+    before_action :authenticate_user!
+
+    def create
+        event = Event.find(params[:event_id])
+        attendance = Attendance.new(event_id: event.id, attendee_id: current_user.id)
+    
+        if attendance.save
+          redirect_to :root and return
+        end
+    end
+
+    def destroy
+        current_user.attendances.find(event: params[:event_id]).destroy
+    end
 end
