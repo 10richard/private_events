bc1b46d1ac8b562725ea2a9ff48453437698716c
Richard Ten <rten04@icloud.com>
2023-06-15 10:36:11 -0400

    Modify params in #create

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,7 +2,7 @@ class AttendancesController < ApplicationController
     before_action :authenticate_user!
 
     def create
-        event = Event.find(params[:event_id])
+        event = Event.find(params[:id])
         attendance = Attendance.new(event_id: event.id, attendee_id: current_user.id, attending: true)
     
         if attendance.save
