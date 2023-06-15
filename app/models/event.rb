0622fca70c00e6b2bcb1ab0c8ecebb272234de7f
Richard Ten <rten04@icloud.com>
2023-06-15 16:28:20 -0400

    Fix scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
-    scope :upcoming -> {where("event_date >= ?", Time.now)}
-    scope :past -> {where("event_date <= ?", Time.now)}
+    scope :upcoming, -> {where("event_date >= ?", Time.now)}
+    scope :past, -> {where("event_date <= ?", Time.now)}
 
     belongs_to :host, class_name: 'User'
     
